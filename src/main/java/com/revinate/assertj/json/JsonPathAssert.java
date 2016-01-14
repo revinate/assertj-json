b0912e6c6fbec3199f50909e9ad78f0b5876aa18
Jorge Lee <jrg.lee@gmail.com>
2016-01-13 21:14:24 -0800

    Add Javadoc

--- src/main/java/com/revinate/assertj/json/JsonPathAssert.java
+++ src/main/java/com/revinate/assertj/json/JsonPathAssert.java
@@ -6,6 +6,11 @@ import org.assertj.core.api.*;
 
 import java.util.List;
 
+/**
+ * Assertions for {@link DocumentContext}.
+ *
+ * @author Jorge Lee
+ */
 public class JsonPathAssert extends AbstractAssert<JsonPathAssert, DocumentContext> {
 
     public JsonPathAssert(DocumentContext actual) {
@@ -16,14 +21,35 @@ public class JsonPathAssert extends AbstractAssert<JsonPathAssert, DocumentConte
         return new JsonPathAssert(documentContext);
     }
 
+    /**
+     * Extracts a JSON text using a JsonPath expression and wrap it in a {@link StringAssert}.
+     *
+     * @param path JsonPath to extract the string
+     * @return an instance of {@link StringAssert}
+     */
     public AbstractCharSequenceAssert<?, String> jsonPathAsString(String path) {
         return Assertions.assertThat(actual.read(path, String.class));
     }
 
+    /**
+     * Extracts a JSON number using a JsonPath expression and wrap it in an {@link IntegerAssert}
+     *
+     * @param path JsonPath to extract the number
+     * @return an instance of {@link IntegerAssert}
+     */
     public AbstractIntegerAssert<?> jsonPathAsInteger(String path) {
         return Assertions.assertThat(actual.read(path, Integer.class));
     }
 
+    /**
+     * Extracts a JSON array using a JsonPath expression and wrap it in a {@link ListAssert}. This method requires
+     * the JsonPath to be <a href="https://github.com/jayway/JsonPath#jsonprovider-spi">configured with Jackson or
+     * Gson</a>.
+     *
+     * @param path JsonPath to extract the array
+     * @param type The type to cast the content of the array, i.e.: {@link String}, {@link Integer}
+     * @return an instance of {@link ListAssert}
+     */
     public <T> AbstractListAssert<?, ? extends List<? extends T>, T> jsonPathAsListOf(String path, Class<T> type) {
         return Assertions.assertThat(actual.read(path, new TypeRef<List<T>>() {
         }));
